68e93e66c331d2d6c63593b0cd853c43f8832945
Lasarussp <001lasarus@gmail.com>
2023-10-13 23:53:40 +0530

    updated api

--- src/Global.js
+++ src/Global.js
@@ -1,2 +1,2 @@
-// export const API = "http://localhost:5000";
-export const API = "https://ecommerce-backend-six-blond.vercel.app";
+//export const API = "http://localhost:5000";
+export const API = "https://e-commerce-backend-eta-six.vercel.app";
